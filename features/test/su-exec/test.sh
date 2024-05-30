38c6e5f0911b7dc90cf35414a111daf85c9b1d0c
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-05-31 02:27:03 +0300

    test: verify that `su-exec` works

--- features/test/su-exec/test.sh
+++ features/test/su-exec/test.sh
@@ -5,4 +5,9 @@ source dev-container-features-test-lib
 
 check "su-exec exists" which su-exec
 
+if [[ "$(id -u || true)" -eq 0 ]]; then
+    check "su-exec works (user)" sh -c 'su-exec bin:daemon id -un | grep -F bin'
+    check "su-exec works (group)" sh -c 'su-exec bin:daemon id -gn | grep -F daemon'
+fi
+
 reportResults
