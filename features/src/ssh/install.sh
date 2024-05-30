a0317a62eb5044565825d94de476cafd0dff6d2b
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-05-31 01:26:53 +0300

    refactor(ssh): support Debiand and Ubuntu, fix bugs

--- features/src/ssh/install.sh
+++ features/src/ssh/install.sh
@@ -9,18 +9,46 @@ if [ "$(id -u || true)" -ne 0 ]; then
     exit 1
 fi
 
+: "${_REMOTE_USER:?"_REMOTE_USER is required"}"
+
 if [ "${ENABLED:-}" = "true" ]; then
     echo '(*) Installing OpenSSH server...'
 
-    apk add --no-cache openssh-server
-    rm -f /etc/conf.d/sshd /etc/init.d/sshd
-    install -D -d -m 0555 -o root -g root /var/empty
+    # shellcheck source=/dev/null
+    . /etc/os-release
+
+    : "${ID:=}"
+    : "${ID_LIKE:=${ID}}"
+    PRIVSEP_DIR=
+
+    case "${ID_LIKE}" in
+        "debian")
+            apt-get update
+            apt-get install -y --no-install-recommends openssh-server
+            apt-get clean
+            rm -rf /var/lib/apt/lists/*
+            update-rc.d ssh remove
+            PRIVSEP_DIR=/run/sshd
+        ;;
+
+        "alpine")
+            apk add --no-cache openssh-server
+            rm -f /etc/conf.d/sshd /etc/init.d/sshd
+            PRIVSEP_DIR=/var/empty
+        ;;
+
+        *)
+            echo "(!) Unsupported distribution: ${ID}"
+            exit 1
+    esac
+
+    install -D -d -m 0555 -o root -g root "${PRIVSEP_DIR}"
     install -d -m 0755 -o root -g root /etc/service
     install -D -m 0755 -o root -g root service-run /etc/sv/openssh/run
     ln -sf /etc/sv/openssh /etc/service/openssh
 
-    if [ -n "${_REMOTE_USER}" ]; then
-        echo "${_REMOTE_USER}:${_REMOTE_USER}" | chpasswd
+    if [ "${_REMOTE_USER}" = 'root' ]; then
+        echo 'PermitRootLogin=yes' > /etc/ssh/sshd_config.d/permit_root_login.conf
     fi
 
     echo 'Done!'
