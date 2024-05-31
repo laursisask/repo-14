2b2c26f71eb91d5d50aa36a7687610158ab673f6
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-05-31 08:30:54 +0300

    refactor(memcached): support Debian and Ubuntu

--- features/src/memcached/install.sh
+++ features/src/memcached/install.sh
@@ -9,23 +9,61 @@ if [ "$(id -u || true)" -ne 0 ]; then
     exit 1
 fi
 
+: "${_REMOTE_USER:?"_REMOTE_USER is required"}"
 : "${ENABLED:=}"
 
 if [ "${ENABLED}" = "true" ]; then
     echo '(*) Installing memcached...'
 
-    if [ -z "${_REMOTE_USER}" ] || [ "${_REMOTE_USER}" = "root" ]; then
-        PHP_USER="www-data"
-    else
-        PHP_USER="${_REMOTE_USER}"
+    # shellcheck source=/dev/null
+    . /etc/os-release
+    : "${ID:=}"
+    : "${ID_LIKE:=${ID}}"
+    RUN_AS=""
+
+    case "${ID_LIKE}" in
+        "debian")
+            PACKAGES="memcached"
+            if ! hash envsubst >/dev/null 2>&1; then
+                PACKAGES="${PACKAGES} gettext"
+            fi
+
+            apt-get update
+            # shellcheck disable=SC2086
+            apt-get install -y --no-install-recommends ${PACKAGES}
+            apt-get clean
+            rm -rf /var/lib/apt/lists/*
+            update-rc.d memcached remove
+            RUN_AS="memcache"
+        ;;
+
+        "alpine")
+            PACKAGES="memcached"
+            if ! hash envsubst >/dev/null 2>&1; then
+                PACKAGES="${PACKAGES} gettext"
+            fi
+
+            # shellcheck disable=SC2086
+            apk add --no-cache ${PACKAGES}
+            RUN_AS="memcached"
+        ;;
+
+        *)
+            echo "(!) Unsupported distribution: ${ID}"
+            exit 1
+        ;;
+    esac
+
+    if [ -d /wp/wp-content ]; then
+        install -m 0644 -o "${_REMOTE_USER}" -g "${_REMOTE_USER}" object-cache.php /wp/wp-content/
     fi
 
-    apk add --no-cache memcached
-    install -D -m 0755 -o root -g root service-run /etc/sv/memcached/run
-    install -d -m 0755 -o root -g root /etc/service
-    install -d -m 0755 -o "${PHP_USER}" -g "${PHP_USER}" /wp
-    install -d -m 0755 -o "${PHP_USER}" -g "${PHP_USER}" /wp/wp-content
-    install -m 0644 -o "${PHP_USER}" -g "${PHP_USER}" object-cache.php /wp/wp-content/
+    export RUN_AS
+
+    install -D -d -m 0755 -o root -g root /etc/service /etc/sv/memcached
+    # shellcheck disable=SC2016
+    envsubst '$RUN_AS' < service-run.tpl > /etc/sv/memcached/run && chmod 0755 /etc/sv/memcached/run
     ln -sf /etc/sv/memcached /etc/service/memcached
+
     echo 'Done!'
 fi
