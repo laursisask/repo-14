01abd951ed06836828ec6adafa827fed4ee59a7c
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-05-31 08:55:41 +0300

    fix: remove unused shellcheck suppressions

--- features/test/dev-tools/test.sh
+++ features/test/dev-tools/test.sh
@@ -3,7 +3,6 @@
 # shellcheck source=/dev/null
 source dev-container-features-test-lib
 
-# shellcheck disable=SC2016
 check "/wp/wp-content/mu-plugins/dev-env-plugin.php exists" test -f /wp/wp-content/mu-plugins/dev-env-plugin.php
 
 reportResults
--- features/test/mailpit/test.sh
+++ features/test/mailpit/test.sh
@@ -3,7 +3,6 @@
 # shellcheck source=/dev/null
 source dev-container-features-test-lib
 
-# shellcheck disable=SC2016
 check "mailpit exists and is executable" test -x /usr/local/bin/mailpit
 check "/etc/sv/mailpit/run exists and is execuatble" test -x /etc/sv/mailpit/run
 check "/etc/service/mailpit is a symlink" test -L /etc/service/mailpit
