1eb984a0a6a6b60bb72afb75fd90049f9f5a87a5
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-05-31 08:07:45 +0300

    test: update global scenarios tests

--- features/test/_global/global-checks.sh
+++ features/test/_global/global-checks.sh
@@ -1,7 +1,8 @@
 #!/vin/bash
 
-check 'php-fpm is running' sh -c 'ps aux | grep php-fpm'
-check 'mailpit is running' sh -c 'ps aux | grep mailpit'
-check 'memcached is running' sh -c 'ps aux | grep memcached'
-check 'mysql is running' sh -c 'ps aux | grep mysqld'
-check 'nginx is running' sh -c 'ps aux | grep nginx'
+check 'php-fpm is running' sudo sh -c 'sv status php-fpm | grep -E ^run:'
+check 'mailpit is running' sudo sh -c 'sv status mailpit | grep -E ^run:'
+check 'memcached is running' sudo sh -c 'sv status memcached | grep -E ^run:'
+check 'mysql is running' sudo sh -c 'sv status mariadb | grep -E ^run:'
+check 'nginx is running' sudo sh -c 'sv status nginx | grep -E ^run:'
+check 'elasticsearch is running' sudo sh -c 'sv status elasticsearch | grep -E ^run:'
