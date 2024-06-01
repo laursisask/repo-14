25f95c98dbdb9021dd790de1c68e3359f6ea9a54
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-06-01 08:05:57 +0300

    refactor(su-exec): use local copy of `su-exec.c`

--- features/src/su-exec/install.sh
+++ features/src/su-exec/install.sh
@@ -33,23 +33,13 @@ case "${ID_LIKE}" in
             PACKAGES="${PACKAGES} tcc"
         fi
 
-        if ! hash wget >/dev/null 2>&1; then
-            PACKAGES="${PACKAGES} wget"
-        fi
-
-        if ! hash update-ca-certificates >/dev/null 2>&1; then
-            PACKAGES="${PACKAGES} ca-certificates"
-        fi
-
         if [ -n "${PACKAGES}" ]; then
             apt-get update
             # shellcheck disable=SC2086
             apt-get install -y --no-install-recommends ${PACKAGES}
         fi
 
-        wget -q https://raw.githubusercontent.com/ncopa/su-exec/master/su-exec.c -O su-exec.c
         cc -O2 su-exec.c -o /usr/local/bin/su-exec
-        rm su-exec.c
 
         if [ -n "${PACKAGES}" ]; then
             # shellcheck disable=SC2086
