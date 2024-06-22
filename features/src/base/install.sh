e2688889de309b9b360b92ce074303599e26c299
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-06-22 04:37:31 +0300

    feat(base): set up eatmydata to accelerate installs

--- features/src/base/install.sh
+++ features/src/base/install.sh
@@ -18,3 +18,77 @@ install -d -D -m 0755 -o "${_REMOTE_USER}" -g "${_REMOTE_USER}" "${HOME_DIR}/.lo
 
 install -m 0644 -o "${_REMOTE_USER}" -g "${_REMOTE_USER}" .bashrc "${HOME_DIR}/.bashrc"
 install -m 0644 -o "${_REMOTE_USER}" -g "${_REMOTE_USER}" 001-welcome.sh "${HOME_DIR}/.local/share/vip-codespaces/login/001-welcome.sh"
+
+# shellcheck source=/dev/null
+. /etc/os-release
+
+: "${ID:=}"
+: "${ID_LIKE:=${ID}}"
+
+PACKAGES=""
+ARCH="$(arch)"
+case "${ID_LIKE}" in
+    "debian")
+        export DEBIAN_FRONTEND=noninteractive
+        PACKAGES=""
+        if ! hash eatmydata >/dev/null 2>&1; then
+            PACKAGES="${PACKAGES} eatmydata"
+        fi
+
+        if ! hash curl >/dev/null 2>&1; then
+            PACKAGES="${PACKAGES} curl"
+        fi
+
+        if ! hash update-ca-certificates >/dev/null 2>&1; then
+            PACKAGES="${PACKAGES} ca-certificates"
+        fi
+
+        if ! hash gpg >/dev/null 2>&1; then
+            PACKAGES="${PACKAGES} gnupg2"
+        fi
+
+        if ! hash lsb_release >/dev/null 2>&1; then
+            PACKAGES="${PACKAGES} lsb-release"
+        fi
+
+        if ! hash envsubst >/dev/null 2>&1; then
+            PACKAGES="${PACKAGES} gettext"
+        fi
+
+        if [ -n "${PACKAGES}" ]; then
+            apt-get update
+            # shellcheck disable=SC2086
+            apt-get install -y --no-install-recommends ${PACKAGES}
+        fi
+
+        mkdir -p /usr/lib/libeatmydata
+        ln -sf -t /usr/lib/libeatmydata/ "/usr/lib/${ARCH}-linux-gnu/libeatmydata.so"*
+    ;;
+
+    "alpine")
+        if ! hash eatmydata >/dev/null 2>&1; then
+            PACKAGES="${PACKAGES} libeatmydata"
+        fi
+
+        if ! hash curl >/dev/null 2>&1; then
+            PACKAGES="${PACKAGES} curl"
+        fi
+
+        if ! hash envsubst >/dev/null 2>&1; then
+            PACKAGES="${PACKAGES} gettext"
+        fi
+
+        if [ -n "${PACKAGES}" ]; then
+            # shellcheck disable=SC2086
+            apk add --no-cache ${PACKAGES}
+        fi
+
+        mkdir -p /usr/lib/libeatmydata
+        ln -sf /usr/lib/libeatmydata.so /usr/lib/libeatmydata/libeatmydata.so
+    ;;
+
+    *)
+        echo "Unsupported distribution: ${ID_LIKE}"
+        exit 1
+    ;;
+esac
