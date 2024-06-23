9c7a29eb212141178aa51281df6907bf6f09e0f2
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-06-23 09:37:28 +0300

    test(php): add tests

---
+++ features/test/php/alpine-82-mcr.sh
@@ -0,0 +1,1 @@
+alpine-81.sh
--- features/test/php/checks.sh
+++ features/test/php/checks.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-check "php-fpm is running" sudo sh -c 'sv status php-fpm | grep -E ^run:'
-sudo sv stop php-fpm
-check "php-fpm is stopped" sudo sh -c 'sv status php-fpm | grep -E ^down:'
-sudo sv start php-fpm
-check "php-fpm is running" sudo sh -c 'sv status php-fpm | grep -E ^run:'
+if hash sv >/dev/null 2>&1; then
+    check "php-fpm is running" sudo sh -c 'sv status php-fpm | grep -E ^run:'
+    sudo sv stop php-fpm
+    check "php-fpm is stopped" sudo sh -c 'sv status php-fpm | grep -E ^down:'
+    sudo sv start php-fpm
+    check "php-fpm is running" sudo sh -c 'sv status php-fpm | grep -E ^run:'
+else
+    check "php-fpm is running" pgrep php-fpm
+fi
+
 check "Port 9000 is open" sh -c 'netstat -lnt | grep :9000 '
 
 check "php can run" php --version
 check "composer can run" composer --version
 
-check "mailpit is running" sudo sh -c 'sv status mailpit | grep -E ^run:'
+check "mailpit is running" pgrep mailpit
 check "Port 1025 is open" sh -c 'netstat -lnt | grep :1025 '
 check "Port 8025 is open" sh -c 'netstat -lnt | grep :8025 '
 
