e259ccbe1d7547b3aaef99bc75d2d86b4a76cebb
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-08-12 12:30:58 +0300

    fix(base): reset `LD_PRELOAD` during installation

--- features/src/base/install.sh
+++ features/src/base/install.sh
@@ -3,6 +3,7 @@
 set -e
 
 PATH=/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin
+export LD_PRELOAD=
 
 if [ "$(id -u || true)" -ne 0 ]; then
     echo 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
