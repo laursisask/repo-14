d543365002db09945c15c3919d4dbe7773a78df0
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-08-12 03:48:53 +0300

    feat(wordpress): install npm packages on "update content" event

--- features/src/wordpress/wordpress-update-content.sh
+++ features/src/wordpress/wordpress-update-content.sh
@@ -3,3 +3,11 @@
 if [ -f composer.json ] && [ -x /usr/local/bin/composer ]; then
     /usr/local/bin/composer install -n || true
 fi
+
+if [ -f package.json ] && hash npm > /dev/null 2>&1; then
+    if [ ! -d node_modules ] && [ -f package-lock.json ]; then
+        npm ci
+    else
+        npm install
+    fi
+fi
